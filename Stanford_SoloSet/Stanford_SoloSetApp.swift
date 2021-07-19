99c6df940f51aceb20ac68763e7f37e4789820ba
Steven Blythe <blythesr@googlemail.com>
2021-07-19 20:36:34 +0100

    Initial Commit

---
+++ Stanford_SoloSet/Stanford_SoloSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stanford_SoloSetApp.swift
+//  Stanford_SoloSet
+//
+//  Created by Steve Blythe on 19/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Stanford_SoloSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
