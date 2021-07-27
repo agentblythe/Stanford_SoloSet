a923cbb8ec78791c1d1bf07c952f242858dfb302
Steven Blythe <blythesr@googlemail.com>
2021-07-27 17:10:21 +0100

    Added Array Extension for identifiable elements

---
+++ Stanford_SoloSet/Extensions/Array+.swift
@@ -0,0 +1,19 @@
+//
+//  Array+Identifiable.swift
+//  Stanford_SoloSet
+//
+//  Created by Steve Blythe on 26/07/2021.
+//
+
+import Foundation
+
+extension Array where Element: Identifiable {
+    func firstIndex (matching: Element)-> Int? {
+          for index in 0..<self.count {
+              if self[index].id == matching.id {
+                  return index
+              }
+          }
+           return nil
+       }
+}
